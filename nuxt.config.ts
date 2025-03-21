2aab24cd9ea292814e5f6f461046b7e2c6c39e4d
Temurbek <temurbekUnical@gmail.com>
2025-03-21 22:52:18 +0500

    fix: deploy error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
+  },
+  nitro: {
+    preset: "vercel",
   },
 });
